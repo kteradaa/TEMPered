8395e476583994ad1ef7a53e26d711e9d3974060
Koichi TERADA <kteradaa@gmail.com>
2019-11-10 19:44:49 +0900

    initial commit

---
+++ examples/CMakeFiles/read-all.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/kterada/temper/TEMPered/examples/read-all.c" "/home/kterada/temper/TEMPered/examples/CMakeFiles/read-all.dir/read-all.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../hidapi/hidapi"
+  "examples/../libtempered"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/kterada/temper/TEMPered/libtempered/CMakeFiles/tempered-shared.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
